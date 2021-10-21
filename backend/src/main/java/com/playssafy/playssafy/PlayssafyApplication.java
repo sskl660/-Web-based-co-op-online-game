2341f0174befc5bcc0fa6e677a7815b01814dbee
SEOPSS\prero <comet00096@gmail.com>
2021-10-21 15:31:16 +0900

    init : Set spring boot

---
+++ backend/src/main/java/com/playssafy/playssafy/PlayssafyApplication.java
@@ -0,0 +1,13 @@
+package com.playssafy.playssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayssafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayssafyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/playssafy/playssafy/PlayssafyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playssafy.playssafy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayssafyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
