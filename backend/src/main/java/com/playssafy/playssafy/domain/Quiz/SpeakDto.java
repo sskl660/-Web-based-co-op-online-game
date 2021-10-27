413fc10a6c1a26ad7f6309090985337d06be647e
Kimtaehyun <sskl600@naver.com>
2021-10-27 15:01:53 +0900

    fix: FIX confilict codes spring 충돌 문제 해결

--- backend/src/main/java/com/playssafy/playssafy/domain/Quiz/SpeakDto.java
+++ backend/src/main/java/com/playssafy/playssafy/domain/Quiz/SpeakDto.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.playssafy.playssafy.domain.Quiz;
 
 import io.swagger.annotations.ApiParam;
@@ -33,39 +32,3 @@ public class SpeakDto {
         public RequestSetSpeakQuiz(){}
     }
 }
-=======
-package com.playssafy.playssafy.domain.Quiz;
-
-import io.swagger.annotations.ApiParam;
-import lombok.*;
-
-@Setter
-@Getter
-@NoArgsConstructor
-public class SpeakDto {
-    @ApiParam(value = "문제 고유넘버 ", required = true, example = "123")
-    private Long quizNum;
-    @ApiParam(value = "문제 내용", required = true, example = "싸피")
-    private String contents;
-
-    public SpeakEntity toEntity(){
-        SpeakEntity speakEntity= SpeakEntity.builder()
-                .quizNum(quizNum)
-                .contents(contents).build();
-        return speakEntity;
-    }
-
-    public SpeakDto(Long quizNum, String contents) {
-        this.quizNum = quizNum;
-        this.contents = contents;
-    }
-
-    @Data
-    @AllArgsConstructor
-    @Builder
-    public static class RequestSetSpeakQuiz{//pk값을 제외하고 주입하기위해 새로 만드는 디티오
-        private String contents;
-        public RequestSetSpeakQuiz(){}
-    }
-}
->>>>>>> aeecd4178ea8e0816dd057ccad6ad4d95f546627
--- backend/src/main/java/com/playssafy/playssafy/domain/Quiz/SpeakEntity.java
+++ backend/src/main/java/com/playssafy/playssafy/domain/Quiz/SpeakEntity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.playssafy.playssafy.domain.Quiz;
 
 import io.swagger.annotations.ApiModel;
@@ -32,38 +31,3 @@ public class SpeakEntity {
         this.contents = contents;
     }
 }
-=======
-package com.playssafy.playssafy.domain.Quiz;
-
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import io.swagger.annotations.ApiParam;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-
-import javax.persistence.*;
-
-@Getter
-@Entity(name = "speak")
-@NoArgsConstructor
-@ApiModel
-public class SpeakEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @ApiParam(value = "문제 넘버",required = true, example = "1")
-    private Long quizNum;
-
-    @Column
-    @NonNull
-    @ApiModelProperty
-    private String contents;
-
-    @Builder
-    public SpeakEntity(Long quizNum, @NonNull String contents) {
-        this.quizNum = quizNum;
-        this.contents = contents;
-    }
-}
->>>>>>> aeecd4178ea8e0816dd057ccad6ad4d95f546627
