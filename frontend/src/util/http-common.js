ec0089e7ae04b0c866938d90f7804f0bf6c47aee
김태현 <sskl600@naver.com>
2021-10-29 01:12:46 +0000

    fix : UPDATE baseURL

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8080',
+  // baseURL: 'http://localhost:8080',
+  baseURL: 'http://3.38.99.194:8080',
   headers: {
     'Content-type': 'application/json',
   },
