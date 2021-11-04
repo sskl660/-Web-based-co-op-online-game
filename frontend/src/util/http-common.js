528ab6ef25706394bd2f635579dbe0d38295c200
Kimtaehyun <sskl600@naver.com>
2021-11-04 19:28:10 +0900

    fix: fix url http to https http 참조 주소 https로 변경

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: 'http://localhost:8080',
-  baseURL: 'http://3.38.99.194:8080',
+  baseURL: 'https://3.38.99.194:8080',
   headers: {
     'Content-type': 'application/json',
   },
