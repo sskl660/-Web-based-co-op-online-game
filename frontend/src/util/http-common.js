da9d0c44a1f1ac24c4781cd81bc6bc15e291d0bf
Kimtaehyun <sskl600@naver.com>
2021-11-04 22:06:55 +0900

    fix: fix spring boot ssl setting spring boot ssl 설정 변경

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: 'http://localhost:8080',
-  baseURL: 'http://k5a302.p.ssafy.io:8080',
+  baseURL: 'https://k5a302.p.ssafy.io:8080',
   headers: {
     'Content-type': 'application/json',
   },
