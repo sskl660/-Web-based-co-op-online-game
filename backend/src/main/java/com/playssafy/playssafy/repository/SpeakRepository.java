27c7fa19a21dc865f00ea8aeeda94bd8489f6e2d
안기훈 <dksdmlwjd1@naver.com>
2021-11-14 01:28:39 +0900

    fix: Fix Speakgame be

--- backend/src/main/java/com/playssafy/playssafy/repository/SpeakRepository.java
+++ backend/src/main/java/com/playssafy/playssafy/repository/SpeakRepository.java
@@ -3,5 +3,5 @@ package com.playssafy.playssafy.repository;
 import com.playssafy.playssafy.dto.speaking.Speaking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpeakRepository extends JpaRepository<Speaking, String> {
+public interface SpeakRepository extends JpaRepository<Speaking, Long> {
 }
