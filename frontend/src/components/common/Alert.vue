6f8237f502bb6b7c16050e78ef8c36b9fc3b3b48
Kimtaehyun <sskl600@naver.com>
2021-11-18 03:49:58 +0900

    MERGE feature/be/service into master

--- frontend/src/components/common/Alert.vue
+++ frontend/src/components/common/Alert.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <div></div>
 </template>
 
 <script>
@@ -7,28 +7,25 @@ import swal from 'sweetalert';
 
 export default {
   name: 'alert',
-  methods:{
-    showAlert(){
+  methods: {
+    showAlert() {
       swal({
-          // className:'alert',
-          title: '문구는 여기 넣으세요.',
-          buttons: {
+        // className:'alert',
+        title: '문구는 여기 넣으세요.',
+        buttons: {
           text: '네',
           cancel: '아니요',
-          },
-      }).then((value)=>{
-          if(value == 'text'){
-              //네 를 눌렀을때 일어날 일은 여기 넣으세요.
-          } else{
-              //취소를 눌렀을 때 일어날 일은 여기 넣으세요.
-          }
-      })
-    }
-}
-}
-
+        },
+      }).then((value) => {
+        if (value == 'text') {
+          //네 를 눌렀을때 일어날 일은 여기 넣으세요.
+        } else {
+          //취소를 눌렀을 때 일어날 일은 여기 넣으세요.
+        }
+      });
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
