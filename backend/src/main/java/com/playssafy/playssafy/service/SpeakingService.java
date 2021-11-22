e0d30d5fdb30ef8ec92a377469b4f807268b3bd4
이장섭 <comet00096@gmail.com>
2021-11-22 06:52:55 +0000

    Update SpeakingService.java

--- backend/src/main/java/com/playssafy/playssafy/service/SpeakingService.java
+++ backend/src/main/java/com/playssafy/playssafy/service/SpeakingService.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-// public class SpeakingService {
+public class SpeakingService {
     @Autowired
     private SpeakGameRepository speakGameRepository;
     @Autowired
